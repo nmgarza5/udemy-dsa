545f53172ad8753c98ad7ef599478a8dbabf0ad4
Nikolas <nikolas.garza5@gmail.com>
2022-09-17 17:04:53 -0700

    completed array chunking

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,56 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
 
+/*
+input: array of nums and chunk size
+return: an array of subarrays - will contain elements from original array
+    but in chunks of the specified amount
+    -example: array: [1, 2, 3, 4], chunk: 2 --> [[ 1, 2], [3, 4]]
+    - any odd amount of elements will result in a subarr with 1 element
+
+edges:
+    - empty input array
+    - chunk size of 0
+    - chunk size greater then initial array size
+
+plan of attack:
+    - initialize new outer array: outer = []
+    - initialize sub array
+    - iterate through input array
+        - push to subarray each ele
+        -when subarray.length = chunk size, push subarray into outer and reset to []
+Time: O(n)
+Space: O(n)
+*/
+
+function chunk(array, size) {
+    // let outer = []
+    // let subarray = []
+
+    // for (let i = 0; i < array.length; i++) {
+
+    //     let lastEle = i === array.length-1
+    //     let current = array[i]
+    //     subarray.push(current)
+
+    //     if (subarray.length === size || lastEle) {
+    //         outer.push(subarray)
+    //         subarray = []
+    //     }
+    // };
+
+    // return outer
+
+    let outer = []
+    let index = 0
+    while (index < array.length) {
+        outer.push(array.slice(index, index + size))
+        index += size
+    }
+
+    return outer
+
+}
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3))
 module.exports = chunk;
