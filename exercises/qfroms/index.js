457f5566015166fb5262527660ac57b0cf1a640d
Nikolas <nikolas.garza5@gmail.com>
2022-09-19 04:27:32 -0700

    completed queue from two stacks

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -4,16 +4,88 @@
 // Queue should implement the methods 'add', 'remove', and 'peek'.
 // For a reminder on what each method does, look back
 // at the Queue exercise.
-// --- Examples
-//     const q = new Queue();
-//     q.add(1);
-//     q.add(2);
-//     q.peek();  // returns 1
-//     q.remove(); // returns 1
-//     q.remove(); // returns 2
 
 const Stack = require('./stack');
 
-class Queue {}
+/*
+notes:
+    - can only use two stacks and stack methods
+    - will move values back and forth between stacks
+
+
+    queue: [ 1, 2, 3, 4 ]
+    main: [ 1, 2, 3, 4 ] side: []
+
+
+plan of attack:
+    - constructor:
+        this.main = new Stack
+        this.temp = new Stack
+
+    - add(ele)
+        - add to the back of the queue
+        - main.add() push on to main
+
+    - remove()
+        - remove from the front of the queue
+        - loop over main
+            - temp.add(main.remove()) -> add the last ele in main to temp
+        - create value = temp.remove() -> remove the first element in the queue
+        - loop over temp
+            - main.add(temp.remove()) -> add everything back to main
+
+    -peek()
+        - return the last elemement in the queue
+        - return main.peek()
+
+*/
+
+class Queue {
+    constructor() {
+        this.main = new Stack()
+        this.temp = new Stack()
+    }
+
+    add(ele) {
+        return this.main.push(ele)
+    }
+
+    remove() {
+        while (this.main.peek()) {
+            this.temp.push(this.main.pop())
+        }
+
+        let first = this.temp.pop()
+
+        while (this.temp.length) {
+            this.main.push(this.temp.pop())
+        }
+        return first
+    }
+
+    peek() {
+        while (this.main.peek()) {
+            this.temp.push(this.main.pop())
+        }
+
+        let peek = this.temp.peek()
+
+        while (this.temp.length) {
+            this.main.push(this.temp.pop())
+        }
+        return peek
+    }
+
+}
+
+// --- Examples
+    const q = new Queue();
+    q.add(1);
+    q.add(2);
+    // console.log(q)
+    console.log(q.peek());  // returns 1
+    console.log(q.remove()); // returns 1
+    console.log(q)
+    q.remove(); // returns 2
 
 module.exports = Queue;
