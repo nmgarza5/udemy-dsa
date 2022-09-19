eec493d968af18a7b02168d57b40e6d10ebee167
Nikolas <nikolas.garza5@gmail.com>
2022-09-18 21:42:48 -0700

    completed fibonacci recursive

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,11 +8,45 @@
 // Example:
 //   fib(4) === 3
 
+/*
+input: integer n
+return: integer nth number of fibonacci series
+
+notes:
+next = curr + prev
+
+
+*/
+
+// ITERATIVE
+// function fib(n) {
+//     let current = 1
+//     let prev = 0;
+//     for (let i = 1; i < n; i++) {
+//         let next = current + prev
+//         prev = current
+//         current = next
+//     }
+//     return current
+// }
+
+// RECURSIVE - my solution
+// function fib(n, count = 1, current = 1, prev = 0) {
+//     if (count === n) return current
+//     let next = current + prev
+//     count++
+
+//     return fib(n, count, next, current)
+// }
+
+
 function fib(n) {
-
-
-
-
+    if (n < 2) return n
+    return fib(n - 1) + fib(n - 2)
 }
 
+
+
+console.log(fib(4))
+console.log(fib(9))
 module.exports = fib;
